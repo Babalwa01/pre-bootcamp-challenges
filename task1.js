520db8fafd3ad09f0766b529cd9c90180d8ae34e
Babalwa01 <babalwambolekwa93@gmail.com>
2020-07-28 17:01:49 +0200

    Task 1 file added

---
+++ task1.js
@@ -0,0 +1,15 @@
+// declaration and initialization of x and y variables
+var x = 0;
+var y = 1;
+
+// prints value of x and y variables to the console 
+console.log(x);
+console.log(y);
+
+// modify value of x and y variables
+x = x + 3;
+y = y + x;
+
+// prints new value of x and y variables to the console
+console.log(x);
+console.log(y);
