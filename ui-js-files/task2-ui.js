c02f6e81e2e1bbaae256016fbea259fb1479b9af
Babalwa01 <babalwambolekwa93@gmail.com>
2020-07-28 20:15:54 +0200

    Task 2 UI JavaScript file added

---
+++ ui-js-files/task2-ui.js
@@ -0,0 +1,13 @@
+// declaration of variables and initialization to calculated value using BODMAS
+var x = 1 + 1 * 2;
+var y = (1 + 1) * 2;
+var z = 1 + (1 * 2);
+var a = 1 + 1 * 2 / 2;
+var b = (1 + 1 * 2) / 2;
+
+// Prints values of the variables to the console - for testing output
+document.write("1 + 1 * 2 = " + x + "<br>");
+document.write("(1 + 1) * 2 = " + y + "<br>");
+document.write("1 + (1 * 2) = " + z + "<br>");
+document.write("1 + 1 * 2 / 2 = " + a + "<br>");
+document.write("(1 + 1 * 2) / 2 = " + b + "<br>");
