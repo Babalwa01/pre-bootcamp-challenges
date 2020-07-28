1c482755613ce085b155b7c4ba5d54cb2f00fe05
Babalwa01 <babalwambolekwa93@gmail.com>
2020-07-28 18:14:16 +0200

    Task 1 JS file for UI added

---
+++ ui-js-files/task1-ui.js
@@ -0,0 +1,15 @@
+// declaration and initialization of x and y variables
+var x = 0;
+var y = 1;
+
+// prints value of x and y variables to the console 
+document.write("x = " + x + "<br>");
+document.write("y = " + y + "<br>");
+
+// modify value of x and y variables
+x = x + 3;
+y = y + x;
+
+// prints new value of x and y variables to the console
+document.write("x + 3 = " + x + "<br>");
+document.write("y + x = " + y);
