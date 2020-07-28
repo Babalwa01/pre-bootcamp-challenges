39e65d82ecb8e03f6de3fbf093a7c9c6ee6cf71a
Babalwa01 <babalwambolekwa93@gmail.com>
2020-07-28 17:08:45 +0200

    Task 2 file added

---
+++ task2.js
@@ -0,0 +1,13 @@
+// declaration of variables and initialization to calculated value using BODMAS
+var x = 1 + 1 * 2;
+var y = (1 + 1) * 2;
+var z = 1 + (1 * 2);
+var a = 1 + 1 * 2 / 2;
+var b = (1 + 1 * 2) / 2;
+
+// Prints values of the variables to the console - for testing output
+console.log(x);
+console.log(y);
+console.log(z);
+console.log(a);
+console.log(b);
